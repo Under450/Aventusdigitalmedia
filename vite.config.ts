239397902bedc33095c5cf4bf1ffc88adb6e0903
Under450 <caj@me.com>
2025-11-14 16:15:16 +0000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
